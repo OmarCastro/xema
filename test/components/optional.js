584d54b43a2e22b6531029992487559b316c372c
Omar Castro <omar.castro.360@gmail.com>
2017-11-13 22:22:26 +0000

    100% coverage on optional schema test, code review

--- test/components/optional.js
+++ test/components/optional.js
@@ -1,4 +1,5 @@
 const optional = require("../../lib/components/optional")
+const array = require("../../lib/components/array")
 const number = require("../../lib/basic-types/number")
 const string = require("../../lib/basic-types/string")
 const boolean = require("../../lib/basic-types/boolean")
@@ -6,8 +7,8 @@ const should = require("chai").should()
 
 describe("testing optional validation", () => {
   
-  it("should not show errors when validating null values", () => optional.validate(null).error.should.eq("value is null"))
-  it("should not show errors when validating undefined values", () => optional.validate(undefined).error.should.eq("value is undefined"))
+  it("should not show errors when validating null values", () => should.not.exist(optional.validate(null).error))
+  it("should not show errors when validating undefined values", () => should.not.exist(optional.validate(undefined).error))
   it("should not show errors when validating any value", () => {
     should.not.exist(optional.validate(1).error)
     should.not.exist(optional.validate({}).error)
@@ -15,91 +16,65 @@ describe("testing optional validation", () => {
     should.not.exist(optional.validate(true).error)
     should.not.exist(optional.validate(Symbol()).error)
   })
-/*
-  it("should show error when validating values that surpass the maximum required length", () => {
-    optional.maxLength(2).validate([null,1,2,3]).error.should.eq("optional length = 4 is bigger than required maximum = 2")
-    optional.maxLength(1).validate(["abc",1,2]).error.should.eq("optional length = 3 is bigger than required maximum = 1")
+  
+  it("should not show errors when validating null with optional string schema", () => {
+    should.not.exist(optional(string).validate(null).error)
   })
   
-  it("should show error when validating values that do not surpass the minimum required length", () => {
-    optional.minLength(2).validate([null]).error.should.eq("optional length = 1 is smaller than required minimum = 2")
-    optional.minLength(10).validate(["abc",1,2]).error.should.eq("optional length = 3 is smaller than required minimum = 10")
+  it("should not show errors when validating null with optional number schema", () => {
+    should.not.exist(optional(number).validate(null).error)
+  })
+  
+  it("should not show errors when validating null with optional boolean schema", () => {
+    should.not.exist(optional(boolean).validate(null).error)
+  })
+  
+  it("should not show errors when validating null with optional array schema", () => {
+    should.not.exist(optional(array).validate(null).error)
+  })
+  
+  it("should not show errors when validating undefined with optional string schema", () => {
+    should.not.exist(optional(string).validate(undefined).error)
+  })
+  
+  it("should not show errors when validating undefined with optional number schema", () => {
+    should.not.exist(optional(number).validate(undefined).error)
+  })
+  
+  it("should not show errors when validating undefined with optional boolean schema", () => {
+    should.not.exist(optional(boolean).validate(undefined).error)
+  })
+  
+  it("should not show errors when validating undefined with optional array schema", () => {
+    should.not.exist(optional(array).validate(undefined).error)
+  })
+  
+  it("should show record schema error when validating invalid numbers", () => {
+    optional(number.max(10)).validate(11).error.should.eq("number = 11 is bigger than required maximum = 10")
+    optional(number.max(30)).validate(30.0001).error.should.eq("number = 30.0001 is bigger than required maximum = 30")
+    optional(number.max(9999999)).validate(+Infinity).error.should.eq("number = Infinity is bigger than required maximum = 9999999")
   })
 
-  it("should show error when validating values that surpass the upper bound", () => {
-    var validationResult = optional.of(number).validate([null])
-    validationResult.error.should.have.length(1)
-    validationResult.error[0].should.deep.equal({ error: 'value is null', index: 0 })
-  })
-
-  
-  it("should not show error when validating valid optionals of a schema", () => {
-    should.not.exist(optional.minLength(1).maxLength(3).of(number.min(0)).validate([20, 500]).error)
-  })
-  
-  it("should not show error when validating valid optionals", () => {
-    should.not.exist(optional.minLength(1).maxLength(20).validate([20, 500, null, undefined, "aa", "test"]).error)
-  })
-  */
 })
 
 describe("testing optional schema subset validation", () => {
-/*
+
    it("should show error when checking with null ", () => {
     const subsetResult = optional.checkSubsetOf(null);
     subsetResult.isSubset.should.be.false
-    subsetResult.reason.should.eq('not comparing with another optionalSchema');
+    subsetResult.reason.should.eq('not comparing with another OptionalSchema');
   })
   
-  
-   it("should show error when max length is bigger than target", () => {
-    const subsetResult1 = optional.maxLength(11).checkSubsetOf(optional.maxLength(10));
-    subsetResult1.isSubset.should.be.false
-    subsetResult1.reason.should.eq('target maximum length = 10 is smaller than source length = 11');
-    
-    const subsetResult2 = optional.maxLength(1101).checkSubsetOf(optional.maxLength(1000));
-    subsetResult2.isSubset.should.be.false
-    subsetResult2.reason.should.eq('target maximum length = 1000 is smaller than source length = 1101');
+  it("should show error when checking with null ", () => {
+    const subsetResult = optional.checkSubsetOf(optional.of(number));
+    subsetResult.isSubset.should.be.false
+    subsetResult.reason.should.eq('source has no record schema while target has NumberSchema');
   })
   
-  it("should show error when min value is smaller than target", () => {
-    const subsetResult1 = optional.minLength(10).checkSubsetOf(optional.minLength(11));
-    subsetResult1.isSubset.should.be.false
-    subsetResult1.reason.should.eq('target minimum length = 11 is bigger than source length = 10');
-    
-    const subsetResult2 = optional.minLength(0).checkSubsetOf(optional.minLength(1));
-    subsetResult2.isSubset.should.be.false
-    subsetResult2.reason.should.eq('target minimum length = 1 is bigger than source length = 0');
-  })
-  
-  it("should show error when target has a record and source not", () => {
-    const subsetResult1 = optional.checkSubsetOf(optional.of(number));
-    subsetResult1.isSubset.should.be.false
-    subsetResult1.reason.should.eq('source has no record schema while target has NumberSchema');
-    
-    const subsetResult2 = optional.checkSubsetOf(optional.of(string));
-    subsetResult2.isSubset.should.be.false
-    subsetResult2.reason.should.eq('source has no record schema while target has StringSchema');
-  })
-  
-   it("should show error when target record that is not a subset", () => {
-    const subsetResult1 = optional.of(number).checkSubsetOf(optional.of(number.max(10)));
-    subsetResult1.isSubset.should.be.false
-    subsetResult1.reason.should.eq('optional record subset: target maximum value = 10 is smaller than source value = Infinity');
-    
-    const subsetResult2 = optional.of(string).checkSubsetOf(optional.of(string.oneOf("Lorem")));
-    subsetResult2.isSubset.should.be.false
-    subsetResult2.reason.should.eq('optional record subset: source does not have enum check while target has { Lorem }');
-  })
-  
-  it("should be a subset max and min boundary inside taget", () => {
-    const subsetResult1 =  optional.minLength(0).maxLength(10).checkSubsetOf(optional);
-    subsetResult1.isSubset.should.be.true;
-    should.not.exist(subsetResult1.reason);
-    
-    const subsetResult2 =  optional.minLength(1).maxLength(5).checkSubsetOf(optional.minLength(0).maxLength(10));
-    subsetResult2.isSubset.should.be.true;
-    should.not.exist(subsetResult2.reason);
+  it("should show error when checking with null ", () => {
+    const subsetResult = optional(string).checkSubsetOf(optional.of(number));
+    subsetResult.isSubset.should.be.false
+    subsetResult.reason.should.eq('optional record subset: not comparing with another StringSchema');
   })
   
   it("should be a subset on equal record schema - number", () => {
@@ -126,48 +101,54 @@ describe("testing optional schema subset validation", () => {
     should.not.exist(subsetResult.reason);
   })
   
-  it("should be a subset on equal record schema - optional of number", () => {
-    const subsetResult =  optional.of(optional.of(number)).checkSubsetOf(optional.of(optional.of(number)));
+  it("should be a subset on equal record schema - array of number", () => {
+    const subsetResult =  optional.of(array.of(number)).checkSubsetOf(optional.of(array.of(number)));
     subsetResult.isSubset.should.be.true;
     should.not.exist(subsetResult.reason);
   })
   
-  it("should be a subset on equal record schema - optional of string", () => {
-    const subsetResult =  optional.of(optional.of(string)).checkSubsetOf(optional.of(optional.of(string)));
+  it("should be a subset on equal record schema - array of string", () => {
+    const subsetResult =  optional.of(array.of(string)).checkSubsetOf(optional.of(array.of(string)));
     subsetResult.isSubset.should.be.true;
     should.not.exist(subsetResult.reason);
   })
   
-  it("should be a subset on equal record schema - optional of boolean", () => {
-    const subsetResult =  optional.of(optional.of(boolean)).checkSubsetOf(optional.of(optional.of(boolean)));
+  it("should be a subset on equal record schema - array of boolean", () => {
+    const subsetResult =  optional.of(array.of(boolean)).checkSubsetOf(optional.of(array.of(boolean)));
     subsetResult.isSubset.should.be.true;
     should.not.exist(subsetResult.reason);
   })
   
-  it("should be a subset on equal record schema - optional of optional", () => {
-    const subsetResult =  optional.of(optional.of(optional)).checkSubsetOf(optional.of(optional.of(optional)));
-    subsetResult.isSubset.should.be.true;
-    should.not.exist(subsetResult.reason);
-  })
-  /*
-  it("should be a subset of target without division check", () => {
-    const subsetResult =  optional.divisibleBy(2).checkSubsetOf(optional);
+  it("should be a subset on equal record schema - array of optional", () => {
+    const subsetResult =  optional.of(array.of(optional)).checkSubsetOf(optional.of(array.of(optional)));
     subsetResult.isSubset.should.be.true;
     should.not.exist(subsetResult.reason);
   })
   
-  it("should be a subset if equal", () => {
-    const subsetResult1 =  optional.min(0).max(10).checkSubsetOf(optional.min(0).max(10));
+  it("should be a subset on equal record schema - array of array", () => {
+    const subsetResult =  optional.of(array.of(array)).checkSubsetOf(optional.of(array.of(array)));
+    subsetResult.isSubset.should.be.true;
+    should.not.exist(subsetResult.reason);
+  })
+  
+  it("should be a subset - optional with record to base", () => {
+    const subsetResult1 =  optional.of(number).checkSubsetOf(optional);
     subsetResult1.isSubset.should.be.true;
     should.not.exist(subsetResult1.reason);
     
-    const subsetResult2 =  optional.min(-9999).max(9999).checkSubsetOf(optional.min(-9999).max(9999));
+    const subsetResult2 =  optional(number).checkSubsetOf(optional);
     subsetResult2.isSubset.should.be.true;
     should.not.exist(subsetResult2.reason);
-    
-    const subsetResult3 =  optional.checkSubsetOf(optional);
-    subsetResult3.isSubset.should.be.true;
-    should.not.exist(subsetResult3.reason);
   })
-*/
+  
+  it("should be a subset if equal", () => {
+    const subsetResult1 =  optional.of(number).checkSubsetOf(optional.of(number));
+    subsetResult1.isSubset.should.be.true;
+    should.not.exist(subsetResult1.reason);
+    
+    const subsetResult2 =  optional(number).checkSubsetOf(optional(number));
+    subsetResult2.isSubset.should.be.true;
+    should.not.exist(subsetResult2.reason);
+  })
+
 })
